d2715834d192eb35ccc696685aa0012bc9b5f22d
ChaoticDrakath <41577255+ChaoticDrakath@users.noreply.github.com>
2019-03-07 12:55:26 +0200

    Update Void.py

--- Void.py
+++ Void.py
@@ -29,10 +29,10 @@ async def on_ready():
 async def background_loop():
     await client.wait_until_ready()
     while not client.is_closed:
-        channel = client.get_channel("552050402703572992")
+        channel = client.get_channel("549332310487465986")
         messages = ["**MOTD: I can't say :maus: correctly.**", "**MOTD: Staff team is here to enforce the rules.**", "**MOTD: Do not give your password to anyone. Stay safe!**"]
         await client.send_message(channel, random.choice(messages))
-        await asyncio.sleep(120)
+        await asyncio.sleep(300)
 
 client.loop.create_task(background_loop())
 
